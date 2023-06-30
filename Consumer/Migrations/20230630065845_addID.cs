eecdc4e06339103a98d292009c585db1e706561f
Mykhailo Vashchuk <Mykhailo.Vashchuk@pointtaken.no>
2023-06-30 11:12:21 +0300

    added some migrations

---
+++ Consumer/Migrations/20230630065845_addID.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Consumer.Migrations
+{
+    /// <inheritdoc />
+    public partial class addID : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
