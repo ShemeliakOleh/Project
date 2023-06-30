883b073671be94f1a0b8bf7a37a6248858abcaf4
Mykhailo Vashchuk <Mykhailo.Vashchuk@pointtaken.no>
2023-06-30 12:52:38 +0300

    Added migrations

---
+++ Front/Migrations/20230630093405_Initial_2.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Front.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ScrappedElements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScrappedElements", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Username = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ScrappedElements");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Front/Migrations/20230630093634_Initial_3.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Front.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial_3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ScrappedElements",
+                table: "ScrappedElements");
+
+            migrationBuilder.RenameTable(
+                name: "ScrappedElements",
+                newName: "scrappedElements");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_scrappedElements",
+                table: "scrappedElements",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_scrappedElements",
+                table: "scrappedElements");
+
+            migrationBuilder.RenameTable(
+                name: "scrappedElements",
+                newName: "ScrappedElements");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ScrappedElements",
+                table: "ScrappedElements",
+                column: "Id");
+        }
+    }
+}
