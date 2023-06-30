185b5440314a9a254d621ec6cb3a3a878552b53c
Mykhailo Vashchuk <Mykhailo.Vashchuk@pointtaken.no>
2023-06-30 09:53:25 +0300

    Added DBContext

---
+++ Consumer/ApplicationDBContext.cs
@@ -0,0 +1,10 @@
+﻿using Consumer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Consumer;
+public class ApplicationDBContext : DbContext
+{
+    public DbSet<ScrappedElement> scrappedElements {  get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite(@"Data Source=C:\sqlite\DB\LiteDB.db");
+}
---
+++ DB/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Scrapper_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB;
+public class ApplicationDBContext : DbContext
+{
+    public DbSet<ScrappedElement> scrappedElements {  get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite(@"Data Source=C:\sqlite\DB\LiteDB.db");
+}
