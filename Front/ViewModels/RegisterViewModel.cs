9e780c4979098b71e30c48891e0822ea1eb30d70
Mykhailo Vashchuk <Mykhailo.Vashchuk@pointtaken.no>
2023-06-30 12:52:49 +0300

    Added View models

---
+++ Front/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Front.ViewModels;
+
+public class RegisterViewModel
+{
+    [Required]
+    public string Username { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm password")]
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; }
+}
+
